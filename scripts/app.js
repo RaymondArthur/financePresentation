f6e7a6dd42a454ebfa63b71ddf0aaecc2fb99aba
RaymondArthur <76266809+RaymondArthur@users.noreply.github.com>
2021-07-02 14:09:37 +1000

    First commit
    Uploading initial files containing barebones structure of app.

---
+++ scripts/app.js
@@ -0,0 +1,9 @@
+// let presentationData;
+// fetch('data/presentation.json')
+//     .then(response => response.json())
+//     .then(data=>{
+//         presentationData = data;
+//         console.log('presentationData is: ',presentationData);
+//         console.log('data is: ',data);
+//     })
+//     .catch(err=>console.log(err));
