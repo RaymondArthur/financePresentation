ab99fcf113a59ef09b2bb52aa993493b2367e3bb
RaymondArthur <76266809+RaymondArthur@users.noreply.github.com>
2021-07-12 20:46:45 +1000

    Updated the content of the presentation

--- data/data.js
+++ data/data.js
@@ -2,8 +2,8 @@ let htmlData = {
     presentationTitle: "Introductory Finance",
     slides: {
         1:{
-            title:"Introductory Finance",
-            content:`<h2>An introduction to financial instruments</h2>
+            title:"Introductory Finance: Fixed Income",
+            content:`<h2>An introduction to finance and fixed income instruments</h2>
             <p>By Raymond Arthur</p>
             <p>This talk is available on my github</p>`
             .replace(/(\n)(\s{2,})/g," ")
@@ -13,9 +13,8 @@ let htmlData = {
             content:`
             <p>This talk will cover the basics of:</p>
             <ul>
-            <li>Time value of money and discounting cash flows</li>
-            <li>Bonds/bills</li>
-            <li>FX spot/forwards/swaps</li>
+                <li>Time value of money and discounting cash flows</li>
+                <li>Fixed income (bonds/bills)</li>
             </ul>`
             .replace(/(\n)(\s{2,})/g," ")
         },
@@ -97,6 +96,140 @@ let htmlData = {
             .replace(/(\n)(\s{2,})/g," ")
         },
         11:{
+            title:"Bonds/Bills",
+            content:`
+            <p>A <em>bond</em> is simply a loan</p>
+            <p>Bond issuers typically:</p>
+            <ul>
+                <li>Borrow an amount for a specified period of time</li>
+                <li>Make periodic interest payments</li>
+                <li>Pay back the amount borrowed at the end</li>
+            </ul>
+            <p>This is essentially the same structure as an interest-only mortgage</p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        12:{
+            title:"Bonds/Bills - Basic Elements",
+            content:`
+            <p>Three basic elements of a bond:</p>
+            <ul>
+                <li><strong>Face value</strong>: the amount being borrowed (i.e. the <em>principal</em>)</li>
+                <li><strong>Coupons (fixed, floating, or indexed)</strong>: the periodic repayments on the bond</li>
+                <li><strong>Maturity date</strong>: the date on which the repayments are completed</li>
+            </ul>
+            <h2>Bills</h2>
+            <p>A <em>bill</em> is essentially a bond with no coupon payments.</p>
+            <p>The interest is factored in to the initial borrowed amount</p>
+            <p>E.g. borrow $9m up front, pay back $10m in 10 years, total interest is $1m</p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        13:{
+            title:"Bonds/Bills - Cash Flow Profile",
+            content:`
+            <p>The cash flows of a fixed interest bond vs bill</p>
+            <img src='/images/bondCashProfile.svg' class='multipleImages'/>
+            <img src='/images/billCashProfile.svg' class='multipleImages'/>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        14:{
+            title:"Bonds/Bills - Valuation",
+            content:`
+            <h2>Present Value of a bond</h2>
+            <p>PV<sub>bond</sub> = CF<sub>1</sub>/(1+r) + CF<sub>2</sub>/(1+r)<sup>2</sup>+ ... + (CF<sub>n</sub>+FV)/(1+r)<sup>n</sup></p>
+            <h2>Present Value of a bill</h2>
+            <p>PV<sub>bill</sub> = FV/(1+r)<sup>n</sup></p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        15:{
+            title:"Bonds/Bills - Valuation & Interest Rates",
+            content:`
+            <p>Using a bill as a stylised example...</p>
+            <p>PV<sub>bill</sub> = FV/(1+r)<sup>n</sup></p>
+            <p class='question'>What happens to the bill's Present Value when the interest rate r changes?</p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        16:{
+            title:"Bonds/Bills - Valuation & Interest Rates",
+            content:`
+            <p>Using a bill as a stylised example...</p>
+            <p>PV<sub>bill</sub> = FV/(1+r)<sup>n</sup></p>
+            <p class='question'>What happens to the bill's Present Value when the interest rate r changes?</p>
+            <p class='question'><mark>If interest rate goes up, PV goes down</mark></p>
+            <p class='answer'><mark>If interest rate goes down, PV goes up</mark></p>
+            <p>The same is true for bonds</p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        17:{
+            title:"Bonds/Bills - Valuation & Interest Rates",
+            content:`
+            <p>Bond $10m face value, 10% coupon rate:</p>
+            <img src='/images/bondPVVersusInterestRates.svg' class='singleImage'>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        18:{
+            title:"Bonds/Bills - Duration & Interest Rate Risk",
+            content:`
+            <p><strong>Duration</strong> describes the relationship between market value and interest rates</p>
+            <p class='answer'><mark>Duration is the first derivative of the PV calculation</mark></p>
+            <p>i.e. dPV/dr = duration</p>
+            <p>Google the mathematics if interested...</p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        19:{
+            title:"Bonds/Bills - Duration & Interest Rate Risk",
+            content:`
+            <p>Duration formula:</p>
+            <p class='answer'><mark>Modified Duration = (Sum(PV<sub>CFi</sub> x T)) / PV) / (1 + ytm) of bond</mark></p>
+            <p>From the formula</p>
+            <ul>
+                <li>Longer dated cash flows increase duration</li>
+                <li>Shorter dated cash flows decrease duration</li>
+            </ul>
+            <p>
+                Controlling duration is one way that the RBA manages interest rate risk on the foreign reserves portfolio.
+            </p>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        20:{
+            title:"Bonds/Bills - Duration & Interest Rate Risk",
+            content:`
+            <p>Put another way:</p>
+            <ul>
+                <li>Increasing duration increases the impact of interest rates changes on portfolio value</li>
+                <li>Decreasing duration decreases the impact of interest rates changes on portfolio value</li>
+            </ul>
+            <p>So:</p>
+            <ul>
+                <li>Worried about rising interest rates? Lower the duration of the portfolio</li>
+                <li>This generally hasn't been a problem since the GFC...</li>
+            </ul>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        20:{
+            title:"Bonds/Bills - Duration & Interest Rate Risk",
+            content:`
+            <p>Putting it all together:</p>
+            <ul>
+                <li>A key worry of bond traders is rising interest rates. Why?</li>        
+                <li>If interest rates go up, bond values go down</li>    
+                <li>What can bond traders do? Lower the duration of the portfolio</li>
+                <li>Lower duration = lower change in portfolio value if interest rates rise. But...</li>
+                <li>This generally hasn't been a problem since the GFC</li>
+            </ul>
+            `
+            .replace(/(\n)(\s{2,})/g," ")
+        },
+        21:{
             title:"Thank you",
             content:`<p>Thank you for your time.</p><p>Contact me if you have any questions and, as always, some good resources are:</p><ul><li>Google</li><li>Wikipedia</li><li>YouTube</li></ul>`
         }
